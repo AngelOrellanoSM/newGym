4fd60630605bb4f6325a8d1ac5403ccccb463e3b
chenaoh <angel.orellano@unmsm.edu.pe>
2024-02-09 20:50:43 -0500

    navbar - complete

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,7 +1,36 @@
+"use client"
+import styles from "./navbar.module.css"
+import { usePathname } from "next/navigation"
+
+import {
+    MdNotifications,
+    MdOutlineChat,
+    MdPublic,
+    MdSearch,
+  } from "react-icons/md";
+
 const Navbar = () => {
+    const pathname = usePathname();
+
     return (
-        <div>
-            Navbar
+        <div className={styles.container}>
+            <div className={styles.title}>
+                {pathname.split("/").pop()}
+            </div>
+
+            <div className={styles.menu}>
+                
+                <div className={styles.icons}>
+                    <div className={styles.alerts}>
+                        <div className={1===2?styles.cantidad:styles.invisible}>0</div>
+                        <MdOutlineChat size={20}></MdOutlineChat>
+                    </div>
+                    <div className={styles.alerts}>
+                        <div className={1===2?styles.cantidad:styles.invisible}>5</div>
+                        <MdNotifications size={20}></MdNotifications>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
