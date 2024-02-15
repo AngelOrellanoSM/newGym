385daf1de261f97e96499b4d939cf7bf05a3485b
chenaoh <angel.orellano@unmsm.edu.pe>
2024-02-15 17:42:02 -0500

    menu - fixed (add compras)

---
+++ app/dashboard/compras/page.jsx
@@ -0,0 +1,11 @@
+import styles from "../../ui/compras/compras.module.css"
+
+const Compras = () => {
+    return (
+        <div>
+            Compras
+        </div>
+    )
+}
+
+export default Compras;
